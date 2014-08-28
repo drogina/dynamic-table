1ec6fed105fb7088e6e42f9e0e0811c0f6fd36d2
Debbie Rogina <deborah.rogina@gmail.com>
2014-08-28 11:06:10 -0400

    added a service and api call to get real data

--- app/table/directives/directives.js
+++ app/table/directives/directives.js
@@ -3,10 +3,24 @@
 
 module.exports = angular.module('tableApp.directives', []).
 	// directive for allergy_list.html
-  directive('showTable', [function() {
+  directive('showTable', function() {
   	return {
   		restrict: 'E',
   		template: require('../main/table.html'),
   		controller: 'TableCtrl'
   	}
-  }]);
+  }).
+  directive('loadingContainer', function() {
+  	return {
+  		restrict: 'A',
+  		scope: false,
+      link: function(scope, element, attrs) {
+          var loadingLayer = angular.element('<div class="loading"></div>');
+          element.append(loadingLayer);
+          element.addClass('loading-container');
+          scope.$watch(attrs.loadingContainer, function(value) {
+              loadingLayer.toggleClass('ng-hide', !value);
+          });
+      }
+  	};
+  });
--- app/table/services/services.js
+++ app/table/services/services.js
@@ -1,3 +1,23 @@
 
 
-module.exports = angular.module('tableApp.services', []);
+module.exports = angular.module('tableApp.services', []).
+
+	factory('Games', function($http) {
+		return {
+			getAll: function(seasonId) {
+				return $http({
+					method: 'GET',
+					url: 'http://www.nicetimeonice.com/api/seasons/' + seasonId + '/games'
+				})
+			},
+			getByTeam: function(sesonnId, teamId) {
+				return $http({
+					method: 'GET',
+					url: 'http://www.nicetimeonice.com/api/seasons/' + seasonId + '/games',
+					params: {
+						team: teamId
+					}
+				})
+			}
+		};
+	});
